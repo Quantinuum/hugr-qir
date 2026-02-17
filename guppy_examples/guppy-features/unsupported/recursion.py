a91d0fb5c45e5717dfb85610a41320ad6535d588
Travis Thompson <travis.thompson@quantinuum.com>
2026-02-17 10:09:14 +0100

    add unsupported recursion example

---
+++ guppy_examples/guppy-features/unsupported/recursion.py
@@ -0,0 +1,28 @@
+import sys
+from typing import no_type_check
+
+from guppylang import guppy
+from guppylang.std.builtins import qubit, result
+from guppylang.std.quantum import h, measure, x
+
+
+@guppy
+@no_type_check
+def recursive_func(q: qubit, n: int) -> None:
+    x(q)
+    if n < 10:  # noqa: PLR2004
+        return recursive_func(q, n + 1)
+    return None
+
+
+@guppy
+@no_type_check
+def main() -> None:
+    q = qubit()
+    h(q)
+    recursive_func(q, 0)
+    result("q", measure(q))
+
+
+if __name__ == "__main__":
+    sys.stdout.buffer.write(main.compile().to_bytes())
