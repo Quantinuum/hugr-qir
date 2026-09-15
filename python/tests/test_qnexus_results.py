4ec02c13038c150326eb0a7e5e1e5483a14bcd9f
Melf <cqc@melf.de>
2026-09-15 21:26:01 +0100

    expand tests

--- python/tests/test_qnexus_results.py
+++ python/tests/test_qnexus_results.py
@@ -24,26 +24,31 @@ def test_backend() -> None:
 
     qs = backendresult_to_qsysresult(backend_result)
 
-    set_reg = set()
-
-    for x in qs[0]:  # check if reg names are in new object
-        set_reg.add(x[0])
-
-    assert set_reg == {
-        "one",
-        "true0",
-        "three",
-        "two",
-        "ten",
-        "false",
-        "qubit0",
-        "integer_value",
-        "true2",
-        "2pow32",
-    }
-
     assert len(qs) == EXPECTED_SHOTS
-    assert len(qs[0]) == EXPECTED_VALUES
+
+    for i in range(EXPECTED_SHOTS):
+        assert len(qs[i]) == EXPECTED_VALUES
+
+        set_reg = set()
+
+        for x in qs[i]:  # check if reg names are in new qsys result
+            set_reg.add(x[0])
+
+        assert set_reg == {
+            "one",
+            "true0",
+            "three",
+            "two",
+            "ten",
+            "false",
+            "qubit0",
+            "integer_value",
+            "true2",
+            "2pow32",
+        }
+
+        for x in qs[i]:
+            assert type(x[1]) is int
 
 
 def test_backend_array() -> None:
@@ -52,12 +57,26 @@ def test_backend_array() -> None:
 
     qs = backendresult_to_qsysresult(backend_result)
 
-    set_reg = set()
-
-    for x in qs[0]:  # check if reg names are in new object
-        set_reg.add(x[0])
-
-    assert set_reg == {"000", "qbs", "ires"}
-
     assert len(qs) == EXPECTED_SHOTS
-    assert len(qs[0]) == EXPECTED_VALUES_ARRAY
+
+    for i in range(EXPECTED_SHOTS):
+        assert len(qs[i]) == EXPECTED_VALUES_ARRAY
+
+        set_reg = set()
+
+        for x in qs[i]:  # check if reg names are in new qsys result
+            set_reg.add(x[0])
+
+        assert set_reg == {"000", "qbs", "ires"}
+
+        for x in qs[i]:
+            if x[0] == "000":
+                assert type(x[1]) is bool
+            elif x[0] == "qbs":
+                assert type(x[1]) is list
+                for y in x[1]:
+                    assert type(y) is bool
+            elif x[0] == "ires":
+                assert type(x[1]) is list
+                for y in x[1]:
+                    assert type(y) is int
